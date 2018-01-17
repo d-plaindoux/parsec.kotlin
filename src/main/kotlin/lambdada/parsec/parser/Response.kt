e415bc06cfa6cc24a713c23638113422a07327ef
Didier Plaindoux <d.plaindoux@free.fr>
2018-01-17 17:25:30 +0100

    Review Response datatype implementing map and flatMap using catamorphism

--- src/main/kotlin/lambdada/parsec/parser/Response.kt
+++ src/main/kotlin/lambdada/parsec/parser/Response.kt
@@ -8,23 +8,20 @@ import lambdada.parsec.io.Reader
 
 // No Monad definition? Back to naive OO approach!
 
-sealed class Response<A> {
-    abstract fun <B> map(f: (A) -> B): Response<B>
-    abstract fun <B> flatMap(f: (A) -> Response<B>): Response<B>
-    abstract fun <B> fold(accept: (Accept<A>) -> B, reject: (Reject<A>) -> B): B
+interface Response<A> {
+    fun <B> fold(accept: (Accept<A>) -> B, reject: (Reject<A>) -> B): B
+
+    fun <B> flatMap(f: (A) -> Response<B>): Response<B> = fold({ f(it.value) }, { Reject(it.consumed) })
+    fun <B> map(f: (A) -> B): Response<B> = fold({ Accept(f(it.value), it.input, it.consumed) }, { Reject(it.consumed) })
 }
 
-class Accept<A>(val value: A, val input: Reader, val consumed: Boolean) : Response<A>() {
-    override fun <B> map(f: (A) -> B): Response<B> = Accept<B>(f(value), input, consumed)
-    override fun <B> flatMap(f: (A) -> Response<B>): Response<B> = f(value)
+data class Accept<A>(val value: A, val input: Reader, val consumed: Boolean) : Response<A> {
     override fun <B> fold(accept: (Accept<A>) -> B, reject: (Reject<A>) -> B): B = accept(this)
 
     override fun toString(): String = "Accept($value)"
 }
 
-class Reject<A>(val consumed: Boolean) : Response<A>() {
-    override fun <B> map(f: (A) -> B): Response<B> = Reject<B>(consumed)
-    override fun <B> flatMap(f: (A) -> Response<B>): Response<B> = Reject<B>(consumed)
+data class Reject<A>(val consumed: Boolean) : Response<A> {
     override fun <B> fold(accept: (Accept<A>) -> B, reject: (Reject<A>) -> B): B = reject(this)
 
     override fun toString(): String = "Reject()"
