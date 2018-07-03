7592548922e63bd997fbc3c8178522f1d41117d7
Didier Plaindoux <d.plaindoux@free.fr>
2018-07-03 07:01:53 +0200

    Code review / Used fold instead of when (...) { ... }

--- src/main/kotlin/lambdada/parsec/parser/Parser+Flow.kt
+++ src/main/kotlin/lambdada/parsec/parser/Parser+Flow.kt
@@ -8,14 +8,19 @@ import lambdada.parsec.parser.Response.Reject
 // Sequence
 // NOTE: [do] comprehension should be better
 
-infix fun <A, B> Parser<A>.then(p: Parser<B>): Parser<Pair<A, B>> = this flatMap { a -> p.map { b -> a to b } }
+infix fun <A, B> Parser<A>.then(p: Parser<B>): Parser<Pair<A, B>> = this flatMap { a -> p map { a to it } }
+
 //
 // Alternate Then
 //
 
-infix fun <A, B> Parser<A>.thenLeft(p: Parser<B>): Parser<A> = this then p map { it.first }
+fun <A, B> Parser<Pair<A, B>>.first(): Parser<A> = this map { it.first }
 
-infix fun <A, B> Parser<A>.thenRight(p: Parser<B>): Parser<B> = this then p map { it.second }
+infix fun <A, B> Parser<A>.thenLeft(p: Parser<B>): Parser<A> = (this then p).first()
+
+fun <A, B> Parser<Pair<A, B>>.second(): Parser<B> = this map { it.second }
+
+infix fun <A, B> Parser<A>.thenRight(p: Parser<B>): Parser<B> = (this then p).second()
 
 //
 // Choice
@@ -29,7 +34,6 @@ infix fun <A> Parser<A>.or(p: Parser<A>): Parser<A> = { reader ->
     }
 }
 
-
 //
 // Kleene operator, optional
 //
@@ -38,16 +42,18 @@ fun <A> opt(p: Parser<A>): Parser<A?> = p map { it as A? } or returns<A?>(null)
 
 // NOTE: Greedy parsers | Prefix i.e. Function vs. Method
 
-// Fold not used / tailrec should be validated if explicit terminal recursion can be detected only
-private tailrec fun <A> optRep(p: Parser<A>, acc: List<A>, consumed: Boolean, charReader: CharReader): Response<List<A>> {
-    val a = p(charReader)
-    return when (a) {
-        is Reject -> Accept(acc, charReader, consumed)
-        is Accept -> optRep(p, acc + a.value, consumed || a.consumed, a.input)
+fun <A> optRep(p: Parser<A>): Parser<List<A>> = {
+    // Fold not used / tailrec should be validated if terminal recursion can be detected only
+    tailrec fun <A> occurrences(p: Parser<A>, acc: List<A>, consumed: Boolean, charReader: CharReader): Response<List<A>> {
+        val a = p(charReader)
+        return when (a) {
+            is Reject -> Accept(acc, charReader, consumed)
+            is Accept -> occurrences(p, acc + a.value, consumed || a.consumed, a.input)
+        }
     }
-}
 
-fun <A> optRep(p: Parser<A>): Parser<List<A>> = { optRep(p, listOf(), false, it) }
+    occurrences(p, listOf(), false, it)
+}
 
 fun <A> rep(p: Parser<A>): Parser<List<A>> = p then optRep(p) map { listOf(it.first) + it.second }
 
@@ -57,7 +63,7 @@ fun <A> rep(p: Parser<A>): Parser<List<A>> = p then optRep(p) map { listOf(it.fi
 
 var eos: Parser<Unit> = {
     if (it.canRead()) {
-        Reject<Unit>(it.location(), false)
+        Reject(it.location(), false)
     } else {
         Accept(Unit, it, false)
     }
--- src/main/kotlin/lambdada/parsec/parser/Parser+Literal.kt
+++ src/main/kotlin/lambdada/parsec/parser/Parser+Literal.kt
@@ -26,13 +26,11 @@ fun not(p: Parser<Char>): Parser<Char> = { reader -> p(reader).fold({ fails<Char
 // Characters parser
 //
 
-fun string(s: String): Parser<String> = s.fold(returns(Unit)) { a, c -> a thenLeft char(c) }.map { s }
+fun string(s: String): Parser<String> = s.fold(returns(Unit)) { a, c -> a thenLeft char(c) } map { s }
 
-fun delimitedString(): Parser<String> {
-    val charExceptColumn = doTry(string("\\\"")) or (not(char('"')).map(Char::toString))
-
-    return (char('"') thenRight optRep(charExceptColumn) thenLeft char('"')).map { it.stringsToString() }
-}
+fun delimitedString(): Parser<String> = char('"') thenRight
+        optRep(doTry(string("\\\"")) or (not(char('"')).map(Char::toString))) thenLeft
+        char('"') map { it.stringsToString() }
 
 //
 // Number parser
@@ -43,9 +41,9 @@ private val STRING_NUMBER: Parser<List<Char>> = rep(charIn('0'..'9'))
 private val STRING_INTEGER: Parser<List<Char>> =
         opt(charIn("-+")) map { it ?: '+' } then STRING_NUMBER map { listOf(it.first) + it.second }
 
-val INTEGER: Parser<Int> = STRING_INTEGER.map { it.charsToInt() }
+val INTEGER: Parser<Int> = STRING_INTEGER map { it.charsToInt() }
 
 val FLOAT: Parser<Float> =
-        (STRING_INTEGER then (opt((char('.') then STRING_NUMBER) map { (s, n) -> (listOf(s) + n) }) map {
+        STRING_INTEGER then (opt(char('.') then STRING_NUMBER map { (s, n) -> (listOf(s) + n) }) map {
             it ?: listOf()
-        })).map { (s, n) -> (s + n).charsToFloat() }
+        }) map { (s, n) -> (s + n).charsToFloat() }
